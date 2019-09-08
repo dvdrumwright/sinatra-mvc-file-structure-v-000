c0f01282048663190f830871fdf0ff7971e87b06
Daniel  Drumwright <danieldrumwright@gmail.com>
2019-09-08 23:56:20 +0000

    Automatically backed up by Learn

---
+++ app/models/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+  attr_accessor :name, :breed, :age
+
+  DOGS = []
+
+def initialization ( :name, :breed, :age)
+  @name = name
+  @breen = breed
+  @age = age
+ end
+
+ self.all
+  DOGS
+ end
+end
